5faddf0f52af50d60588acced9c5dbd7e4beb74f
captain-lemming <46821340+captain-lemming@users.noreply.github.com>
2020-12-18 10:48:12 -0600

    Update utils.js

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -97,7 +97,7 @@ const getProlificId = (data) => {
     }
   }
 
-  const prolificId = getQueryVariable("PROLIFIC_PID");
+  const prolificId = getQueryVariable("PID");
   // const sessionId = getQueryVariable("SESSION_ID");
   // const studyId = getQueryVariable("STUDY_ID");
 
