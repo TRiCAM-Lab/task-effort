54451daf1ea027e7d953006dc9c5a23ba90fdbab
Rebecca Kazinka <rkazinka@gmail.com>
2020-12-22 13:57:31 -0600

    Print choice stats for a participant

--- retrieve/GetData.py
+++ retrieve/GetData.py
@@ -1,3 +1,5 @@
+from collections.abc import Mapping
+
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
@@ -9,8 +11,19 @@ firebase_admin.initialize_app(cred)
 
 db = firestore.client()
 
-docs = db.collection(u'db_pilot_test').document(u'test').collection(u'data').stream()
+trials = db.collection(u'db_pilot_test').document(u'5ad90f90dec767000128a5f4').collection(u'data').stream()
 
-for doc in docs:
-    # output each doc into a file
-    print(f'{doc.id} => {doc.to_dict()}')
+call_function_trials = [trial.to_dict() for trial in trials if trial.to_dict().get("trial_type") == "call_function"]
+choice_trials = [
+    trial for trial in call_function_trials
+    if isinstance(trial.get("value"), Mapping) and trial.get("value").get("subtrial_type") == "choice"
+]
+
+no_choice_trials = [
+    trial for trial in choice_trials
+    if trial.get("value").get("key") == 0
+]
+
+print(f"number of choice trials: {len(choice_trials)}")
+print(f"number with no key pressed: {len(no_choice_trials)}")
+print(f"percent with no key pressed: {100 * (len(no_choice_trials) / len(choice_trials)):.2f}%")
