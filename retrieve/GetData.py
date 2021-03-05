4cecd0719407c5c62f5e7efec130c9824a82beec
Rebecca Kazinka <rkazinka@gmail.com>
2021-03-04 18:21:40 -0600

    Update GetData to iterate by subject

--- retrieve/GetData.py
+++ retrieve/GetData.py
@@ -11,59 +11,58 @@ firebase_admin.initialize_app(cred)
 
 db = firestore.client()
 
-trials = db.collection(u'db_pilot_test').document(u'5fe0ad8e0f62f49c23ceb6bc').collection(u'data').stream()
+subs = db.collection(u'db_pilot_test').stream()
 
-call_function_trials = [trial.to_dict() for trial in trials if trial.to_dict().get("trial_type") == "call_function"]
-choice_trials = [
-    trial for trial in call_function_trials
-    if trial.get("code") == 40
-    # if isinstance(trial.get("value"), Mapping) and trial.get("value").get("subtrial_type") == "choice"
-]
+for sub in subs:
+    print(f'{sub.id}, {sub.to_dict()}')
 
-no_choice_trials = [
-    trial for trial in choice_trials
-    if trial.get("value").get("key") == 0
-]
+    # Get a collection from anywhere
+    trials = db.collection(u'db_pilot_test', sub.id, u'data').stream()
 
-print(f"number of choice trials: {len(choice_trials)}")
-print(f"number with no key pressed: {len(no_choice_trials)}")
-print(f"percent with no key pressed: {100 * (len(no_choice_trials) / len(choice_trials)):.2f}%")
+    # Get a document from anywhere
+    # trial_foo = db.document(u'db_pilot_test', sub.id, u'data', u'trial_1').get()
 
-#Extract # of trials where they chose the easy balloon
-# involves the choice trials and value: high_effort true vs. false and key: 81 (q) vs. 80 (p)
+    call_function_trials = [trial.to_dict() for trial in trials if trial.to_dict().get("trial_type") == "call_function"]
+    choice_trials = [
+        trial for trial in call_function_trials
+        if trial.get("code") == 40
+        # if isinstance(trial.get("value"), Mapping) and trial.get("value").get("subtrial_type") == "choice"
+    ]
 
-high_effort_trials = [
-    trial for trial in choice_trials
-    if isinstance(trial.get("value"), Mapping) and trial.get("value").get("high_effort")
-]
+    no_choice_trials = [
+        trial for trial in choice_trials
+        if trial.get("value").get("key") == 0
+    ]
 
-low_effort_trials = [
-    trial for trial in choice_trials
-    if isinstance(trial.get("value"), Mapping) and trial.get("value").get("high_effort") == False
-]
+    print(f"number of choice trials: {len(choice_trials)}")
+    print(f"number with no key pressed: {len(no_choice_trials)}")
+    print(f"percent with no key pressed: {100 * (len(no_choice_trials) / len(choice_trials)):.2f}%")
 
-print(f"number of high effort trials: {len(high_effort_trials)}")
-print(f"number of low effort trials: {len(low_effort_trials)}")
+    #Extract # of trials where they chose the easy balloon
+    # involves the choice trials and value: high_effort true vs. false and key: 81 (q) vs. 80 (p)
 
-q_trials = [
-    trial for trial in choice_trials
-    if isinstance(trial.get("value"), Mapping) and trial.get("value").get("key") == 81
-]
+    high_effort_trials = [
+        trial for trial in choice_trials
+        if isinstance(trial.get("value"), Mapping) and trial.get("value").get("high_effort")
+    ]
 
-p_trials = [
-    trial for trial in choice_trials
-    if isinstance(trial.get("value"), Mapping) and trial.get("value").get("key") == 80
-]
+    low_effort_trials = [
+        trial for trial in choice_trials
+        if isinstance(trial.get("value"), Mapping) and trial.get("value").get("high_effort") == False
+    ]
 
-print(f"number of q trials: {len(q_trials)}")
-print(f"number of p trials: {len(p_trials)}")
+    print(f"number of high effort trials: {len(high_effort_trials)}")
+    print(f"number of low effort trials: {len(low_effort_trials)}")
 
-# Extract average number of pumps
-# pump_trials = [
-#     trial for trial in call_function_trials
-#     if trial.get("code") == 50
-# ]
-#
-# pump_num = [
-#     trial for trial in pump_trials
-#     if isinstance(trial.get("value"), Mapping) and trial.get("value").get("pumps")
+    q_trials = [
+        trial for trial in choice_trials
+        if isinstance(trial.get("value"), Mapping) and trial.get("value").get("key") == 81
+    ]
+
+    p_trials = [
+        trial for trial in choice_trials
+        if isinstance(trial.get("value"), Mapping) and trial.get("value").get("key") == 80
+    ]
+
+    print(f"number of q trials: {len(q_trials)}")
+    print(f"number of p trials: {len(p_trials)}")
